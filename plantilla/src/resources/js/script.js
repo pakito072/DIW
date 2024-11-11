73a1f7338ecc7c112872fca3227cbc48b7d5905d
pakito072 <105859870+pakito072@users.noreply.github.com>
2024-11-11 12:47:53 +0100

    interface 0.1
    Elementos html añadidos y comienzo de creacion del srcipt

---
+++ plantilla/src/resources/js/script.js
@@ -0,0 +1,21 @@
+const bgVid = document.getElementById("bgVid")
+const bgMusic = document.getElementById("bgMusic")
+const muteBtn = document.getElementById("muteBtn")
+const volPlusBtn = document.getElementById("vol+Btn")
+const volLessBtn = document.getElementById("vol-Btn")
+const statServText = document.getElementById("statServText")
+
+let isMuted = false
+let volume = 1
+const songs = [
+  //Aqui van los enlaces de las canciones
+]
+
+function switchSong() {
+  
+}
+
+function updateStatusServer() {
+  const states = ["", "", ""]
+}
+
