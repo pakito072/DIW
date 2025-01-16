fab9fff769d2d7586480ae6076bdc612a08005c6
Francisco <paquitotejeroangel@gmail.com>
2025-01-16 14:11:34 +0100

    progress bar implemented

--- PaginaWebInteractiva/resources/js/script.js
+++ PaginaWebInteractiva/resources/js/script.js
@@ -1,4 +1,11 @@
 
+window.addEventListener('scroll', function() {
+  const progressBar = document.getElementById('bar').querySelector('.progress-bar');
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  progressBar.style.height = scrollPercent + '%';
+});
 
 
 
